2fe57507d732c29c20cfe982dedb66696b90645a
고해석 <wfos3241@hanmail.net>
2025-07-31 11:05:39 +0900

    Fix : 패키지명/클래스파일명 수정 (borad -> board)

--- SpringBootBoard/src/test/java/com/smhrd/board/SpringBootBoardApplicationTests.java
+++ SpringBootBoard/src/test/java/com/smhrd/board/SpringBootBoardApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootBoradApplicationTests {
+class SpringBootBoardApplicationTests {
 
 	@Test
 	void contextLoads() {
